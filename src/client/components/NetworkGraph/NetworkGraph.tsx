f7c70500d87765eb73db8f0c90a9b18a43f83efc
GianMarcoAlagna <marcoalagna1@gmail.com>
2024-03-02 13:06:48 -0600

    Added zoom support

--- src/client/components/NetworkGraph/NetworkGraph.tsx
+++ src/client/components/NetworkGraph/NetworkGraph.tsx
@@ -3,6 +3,7 @@ import { ResponsiveNetwork } from '@nivo/network';
 import { useSelector } from 'react-redux';
 import * as d3 from 'd3';
 import { ZoomBehavior, ZoomedElementBaseType, zoomIdentity } from 'd3-zoom';
+import './NetworkGraph.css';
 
 export const NetworkGraph = () => {
   const nodes = useSelector((state: any) => state.main.networkData.nodes);
@@ -18,45 +19,23 @@ export const NetworkGraph = () => {
 
   useEffect(() => {
     const svgElement = d3.select(svgRef.current);
-    // // Define the SVG element to apply the zoom behavior
-    // const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> =
-    //   d3.select(svgRef.current);
-
-    // // Define the width and height of the SVG for zoom scaling limits
-    // const width: number = +svg.attr('width');
-    // const height: number = +svg.attr('height');
-
-    // // Create a zoom handler
-    // const zoomHandler: ZoomBehavior<Element, unknown> = d3
-    //   .zoom<ZoomedElementBaseType, unknown>()
-    //   .scaleExtent([1, 8]) // Set the scale extent to control zoom levels
-    //   .on('zoom', (event) => {
-    //     // This function defines what happens during the zoom event
-    //     // For example, scaling and translating an SVG group (<g>)
-    //     svg.selectAll('g').attr('transform', event.transform.toString());
-    //   });
-    // // Apply the zoom behavior to the SVG element
-    // svg
-    //   .call(zoomHandler)
-    //   .call(zoomHandler.transform, zoomIdentity) // Optionally set an initial zoom state
-    // const zoomHandler: d3.ZoomBehavior<Element, unknown> = d3.zoom().on('zoom', (event) => {
-    //   svgElement.attr('transform', event.transform);
-    // });
-    // svgElement.call(zoomHandler);
     function zoomed(event: any) {
-      console.log(event);
-      svgElement.attr('transform', event.transform);
+      // console.log(event);
+      svgElement.selectAll('g').attr('transform', event.transform.toString());
     }
+    
     const zoom = d3.zoom().on('zoom', zoomed) as any;
     svgElement.call(zoom);
     return () => {
       svgElement.on('.zoom', null);
+      svgElement.on('.drag', null);
     };
   }, []); // empty dependency array to run the effect only once
 
   return (
     <div
       ref={svgRef}
+      id="network-graph-container"
       style={{ width: '100%', height: '100%', position: 'relative' }}
     >
       <ResponsiveNetwork
--- src/client/pages/Main/Main.tsx
+++ src/client/pages/Main/Main.tsx
@@ -1,13 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { NavBar } from '../../components/NavBar';
 
 export const Main = () => {
+  const renderNav =
+    useLocation().pathname !== '/login' && useLocation().pathname !== '/signup';
   return (
-    <>
-      <NavBar />
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+    }}>
+      {renderNav && <NavBar />}
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
