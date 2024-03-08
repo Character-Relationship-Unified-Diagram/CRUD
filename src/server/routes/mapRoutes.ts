de8ccbe1ca27d4c4b7e19a0b3a613b80afc01005
Ari Anguiano <crispulum@gmail.com>
2024-03-07 18:59:40 -0600

    AA: added error catching and getPublicMap route

--- src/server/routes/mapRoutes.ts
+++ src/server/routes/mapRoutes.ts
@@ -26,4 +26,8 @@ Router.get('/getMap', mapController.getMap, (_req: Request, res: Response) => {
     return res.status(200).json(res.locals);
 })
 
+Router.get('/getPublicMap', mapController.getPublicMap, (_req: Request, res: Response) => {
+    return res.status(200).json(res.locals);
+})
+
 export default Router;
