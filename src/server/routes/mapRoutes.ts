0323569d3b4a2b576a50e9f37ea63ba2032b5a01
ash-t-luu <ashleyluu87@gmail.com>
2024-03-07 19:50:45 -0800

    working and tested functionality for delete character relationship and working route

--- src/server/routes/mapRoutes.ts
+++ src/server/routes/mapRoutes.ts
@@ -45,4 +45,8 @@ Router.post('/create-character-relationship', mapController.addCharacterRelation
     return res.status(200).json(res.locals.character_statuses);
 });
 
+Router.delete('/delete-character-relationship', mapController.deleteCharacterRelationship, mapController.getMap, (_req: Request, res: Response) => {
+    return res.status(200).json(res.locals);
+});
+
 export default Router;
